18af88af948c1a9d6027366e5afb9dcc8ba3ad81
Melissa Pastore <melissa.pastore@gmail.com>
2021-04-16 21:19:45 -0400

    refactor: updated navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (

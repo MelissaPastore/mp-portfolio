bf2a535d9d02cfa080b174129f878491330ab6c5
Melissa Pastore <melissa.pastore@gmail.com>
2021-04-21 23:09:05 -0400

    style: updated about component

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,19 +4,17 @@ const Home = () => {
   return (
     <div id="home">
       <img id="home-img" src="src/assets/MP-portfolio.png"></img>
-      <div>
-        <h1>
-          Hi, I'm Melissa Pastore, a software engineer and web developer
-          specializing in full stack JavaScript
-        </h1>
+      <h2>
+        Hi, I'm Melissa Pastore, a software engineer and web developer
+        specializing in full stack JavaScript
+      </h2>
 
-        <h2>
-          After studying journalism and working in public relations and
-          communications, I made a career transition into software engineering.
-          I love solving problems and telling stories through web and software
-          development.
-        </h2>
-      </div>
+      <h2>
+        After studying journalism and working in public relations and
+        communications, I made a career transition into software engineering. I
+        love solving problems and telling stories through web and software
+        development.
+      </h2>
     </div>
   );
 };

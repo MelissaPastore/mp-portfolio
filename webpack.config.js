9ce5843694d97ce1e1a41da384212f8b0bfd12ac
Melissa Pastore <melissa.pastore@gmail.com>
2021-04-29 10:54:31 -0400

    changed webpack config to use file loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
         exclude: /node_modules/,
         use: {
-          loader: "url-loader?limit=100000",
+          loader: "file-loader",
         },
       },
     ],

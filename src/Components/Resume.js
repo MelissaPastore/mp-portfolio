cd4d79f357fbf244c5f50e65253a84c0c5d448e3
Melissa Pastore <melissa.pastore@gmail.com>
2021-05-09 15:07:24 -0400

    updates styling, media queries and cleans up css

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -13,7 +13,7 @@ const Resume = () => {
     <div id="resume">
       <div id="pdf">
         <a
-          className="link"
+          className="pdf-link"
           href="https://firebasestorage.googleapis.com/v0/b/portfolio-d7d24.appspot.com/o/MelissaPastore2021.pdf?alt=media&token=57c819f3-2a7b-4729-8169-b2d9c8e933b4"
           download
         >
@@ -27,19 +27,18 @@ const Resume = () => {
           contentArrowStyle={{ borderRight: "7px solid  black" }}
           icon={<SchoolIcon />}
         >
-          <h3 className="vertical-timeline-element-title">Education</h3>
+          <h3>Education</h3>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           contentStyle={{ background: "#13505B", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  black" }}
-          className="vertical-timeline-element--education"
+          contentArrowStyle={{ borderRight: "7px solid  #13505B" }}
           date="October 2020 - February 2021"
           dateClassName={"date"}
           iconStyle={{ background: "black", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <h3 className="vertical-timeline-element-title">Fullstack Academy</h3>
-          <h4 className="vertical-timeline-element-subtitle">
+          <h3>Fullstack Academy</h3>
+          <h4>
             Web Development Fellowship, a partnership with the NYC Tech Talent
             Pipeline
           </h4>
@@ -51,17 +50,12 @@ const Resume = () => {
         </VerticalTimelineElement>
         <VerticalTimelineElement
           contentStyle={{ background: "#13505B", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  black" }}
-          className="vertical-timeline-element--education"
+          contentArrowStyle={{ borderRight: "7px solid  #13505B" }}
           iconStyle={{ background: "black", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            Syracuse University
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            BA in Broadcast Journalism and Policy Studies
-          </h4>
+          <h3>Syracuse University</h3>
+          <h4>BA in Broadcast Journalism and Policy Studies</h4>
           <p>Graduated Magna Cum Laude</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
@@ -70,22 +64,19 @@ const Resume = () => {
           contentArrowStyle={{ borderRight: "7px solid  black" }}
           icon={<WorkIcon />}
         >
-          <h3 className="vertical-timeline-element-title">Work Experience</h3>
+          <h3>Work Experience</h3>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
           contentStyle={{ background: "#13505B", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "black" }}
+          contentArrowStyle={{ borderRight: "7px solid  #13505B" }}
           date="February 2021 - May 2021"
           dateClassName={"date"}
           iconStyle={{ background: "black", color: "#fff" }}
           icon={<WorkIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            Software Engineering Teaching Fellow
-          </h3>
-          <h3 className="vertical-timeline-element-title">Fullstack Academy</h3>
-          <h4 className="vertical-timeline-element-subtitle">New York, NY</h4>
+          <h3>Software Engineering Teaching Fellow</h3>
+          <h3>Fullstack Academy</h3>
+          <h4>New York, NY</h4>
           <p>
             + Provided ongoing mentoring and support to students through small
             group meetings and office hours + Assisted students with debugging
@@ -96,19 +87,14 @@ const Resume = () => {
         <VerticalTimelineElement
           contentStyle={{ background: "#13505B", color: "#fff" }}
           contentArrowStyle={{ borderRight: "7px solid  #13505B" }}
-          className="vertical-timeline-element--work"
           date="October 2018-June 2020"
           dateClassName={"date"}
           iconStyle={{ background: "black", color: "#fff" }}
           icon={<WorkIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            Director of Public Relations
-          </h3>
-          <h3 className="vertical-timeline-element-title">
-            Iconix Brand Group
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">New York, NY</h4>
+          <h3>Director of Public Relations</h3>
+          <h3>Iconix Brand Group</h3>
+          <h4>New York, NY</h4>
           <p>
             + Led public relations and communications efforts across a portfolio
             of more than 20 brands including Danskin, Umbro and London Fog +
@@ -123,17 +109,14 @@ const Resume = () => {
         <VerticalTimelineElement
           contentStyle={{ background: "#13505B", color: "#fff" }}
           contentArrowStyle={{ borderRight: "7px solid  #13505B" }}
-          className="vertical-timeline-element--work"
           date="February 2016-October 2018"
           dateClassName={"date"}
           iconStyle={{ background: "black", color: "#fff" }}
           icon={<WorkIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            Senior Manager, Public Relations
-          </h3>
-          <h3 className="vertical-timeline-element-title">Lacoste USA</h3>
-          <h4 className="vertical-timeline-element-subtitle">New York, NY</h4>
+          <h3>Senior Manager, Public Relations</h3>
+          <h3>Lacoste USA</h3>
+          <h4>New York, NY</h4>
           <p>
             + Reported directly to the CEO during the first year to improve and
             execute a U.S. public relations strategy that increased impressions
@@ -147,19 +130,14 @@ const Resume = () => {
         <VerticalTimelineElement
           contentStyle={{ background: "#13505B", color: "#fff" }}
           contentArrowStyle={{ borderRight: "7px solid  #13505B" }}
-          className="vertical-timeline-element--work"
           date="June 2006-January 2016"
           dateClassName={"date"}
           iconStyle={{ background: "black", color: "#fff" }}
           icon={<WorkIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            Public Relations Assistant to Public Relations Manager
-          </h3>
-          <h3 className="vertical-timeline-element-title">
-            Swarovski North America Limited
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">New York, NY</h4>
+          <h3>Public Relations Assistant to Public Relations Manager</h3>
+          <h3>Swarovski North America Limited</h3>
+          <h4>New York, NY</h4>
           <p>
             + Produced and executed U.S. media outreach strategy that generated
             more than 100 million media impressions in top-tier publications

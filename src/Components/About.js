aa272d6a8a08109bc8b9cc2f14e2dbb2f1d916ba
Melissa Pastore <64283754+MelissaPastore@users.noreply.github.com>
2026-08-21 21:29:49 -0400

    Fix the About image by replacing the broken Firebase URL with a local asset in src/assets

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import portrait from "../assets/MPAbout.png";
 
 const About = () => {
   return (
@@ -6,15 +7,15 @@ const About = () => {
       <img
         id="home-img"
         alt="Site creator's image"
-        src="https://firebasestorage.googleapis.com/v0/b/portfolio-d7d24.appspot.com/o/MPAbout.png?alt=media&token=970aedbe-0ba2-4412-bed7-ec8db60c2646"
+        src={portrait}
       />
       <p className="about-text">
-        I'm currently a software engineer at GitHub. 
-        I made a career change into software engineering and web development, after
-        spending more than 10 years working in public relations, marketing and
-        communications for retail and fashion brands. To help me make this
-        transition, I completed the Web Development Fellowship at Fullstack
-        Academy, a partnership with the NYC Tech Talent Pipeline.
+        I'm currently a software engineer at GitHub. I made a career change into
+        software engineering and web development, after spending more than 10
+        years working in public relations, marketing and communications for retail
+        and fashion brands. To help me make this transition, I completed the Web
+        Development Fellowship at Fullstack Academy, a partnership with the NYC
+        Tech Talent Pipeline.
       </p>
       <p className="about-text">
         My goal is to use my tech skills, along with the communication,
